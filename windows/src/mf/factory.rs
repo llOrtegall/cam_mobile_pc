983789497878588a7ad815fb74b45255364470db
llortegall <ivanortega_97@hotmail.com>
2026-03-08 00:23:31 -0500

    feat: Update IClassFactory implementation to use AndroidCamActivate for IMFActivate interface

--- windows/src/mf/factory.rs
+++ windows/src/mf/factory.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use log::info;
 use windows::Win32::Foundation::BOOL;
@@ -6,12 +6,12 @@ use windows::Win32::Media::MediaFoundation::*;
 use windows::Win32::System::Com::{IClassFactory, IClassFactory_Impl};
 use windows::core::*;
 
-use super::source::AndroidCamSource;
+use super::activate::AndroidCamActivate;
 use super::types::StreamShared;
 
-/// IClassFactory for AndroidCamSource, used when the new IMFVirtualCamera interface
+/// IClassFactory for AndroidCamActivate, used when the new IMFVirtualCamera interface
 /// (IID 1c08a864) is present.  In that case, IMFVirtualCamera::Start() calls
-/// CoCreateInstance(sourceId CLSID) to instantiate the media source.
+/// CoCreateInstance(sourceId CLSID) and requests IMFActivate from the factory.
 /// We register this factory via CoRegisterClassObject before calling Start so
 /// that Frame Server finds the running factory instead of trying to launch a new
 /// process or looking in the registry.
@@ -37,16 +37,14 @@ impl IClassFactory_Impl for AndroidCamSourceFactory_Impl {
                 g.data4[0], g.data4[1],
                 g.data4[2], g.data4[3], g.data4[4], g.data4[5], g.data4[6], g.data4[7]
             );
-            let event_queue: IMFMediaEventQueue = MFCreateEventQueue()?;
-            let source_obj = AndroidCamSource {
-                shared: Arc::clone(&self.shared),
-                presentation_desc: self.presentation_desc.clone(),
-                stream_desc: self.stream_desc.clone(),
-                event_queue,
-                stream: Mutex::new(None),
-            };
-            let source: IMFMediaSourceEx = source_obj.into();
-            let unk: IUnknown = source.cast()?;
+
+            let activate_obj = AndroidCamActivate::new(
+                Arc::clone(&self.shared),
+                self.presentation_desc.clone(),
+                self.stream_desc.clone(),
+            )?;
+            let activate: IMFActivate = activate_obj.into();
+            let unk: IUnknown = activate.cast()?;
             let hr = unk.query(riid, ppv);
             info!("[vcam] CreateInstance QI result: hr={:#010x}", hr.0 as u32);
             hr.ok()
